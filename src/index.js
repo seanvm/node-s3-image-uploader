d65a39ef5192a11e6bfa823f8f9aa0f6060a8400
Seanvm <Sean@vanmulligen.ca>
2018-11-12 15:30:58 -0800

    Add basic validation for image

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const fileType = require('file-type');
 
 class Uploader {
-  constructor(bucketName, options) {
-    if (typeof bucketName === 'undefined') throw new Error('bucketName is required');
+  constructor(bucketName, options = {}) {
+    if (typeof bucketName === 'undefined') throw new Error('bucketName is required.');
 
     this.bucketName = bucketName;
     this.options = options;
+  }
+
+  upload(image) {
+    this.setupImage(image);
+  }
+
+  setupImage(image) {
+    let buffer = Buffer.from(image, 'base64');
+    let fileMime = fileType(buffer);
+    this.validateImage(fileMime);
+  }
+
+  validateImage(fileMime) {
+    if (fileMime === null) {
+      throw new Error('Invalid file type.');
+    }
   }
 };
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,21 @@
 import Uploader from '../src/index.js';
 
+import statics from './resources/statics';
+
 const expect = require('chai').expect;
 
 describe('Uploader', () => {
   describe('constructor', () => {
     it('should throw an error for missing bucketName parameter', () => {
       var newUploaderFcn = () => new Uploader();
-      expect(newUploaderFcn).to.throw('bucketName is required');
+      expect(newUploaderFcn).to.throw('bucketName is required.');
+    });
+  });
+
+  describe('upload()', () => {
+    it('should throw an error for an invalid file type', () => {
+      var uploader = new Uploader('xyz.com');
+      expect(() => uploader.upload(statics.invalidImage)).to.throw('Invalid file type.');
     });
   });
 });
